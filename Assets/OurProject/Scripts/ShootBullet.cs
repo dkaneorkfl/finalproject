b5e76ebb2d44383ef5a892af8e94d42e35cea14c
dkaneorkfl <jtre159357@gmail.com>
2018-11-12 14:57:07 +0900

    1112오후3:00
    프로젝트 시동용 파일업로드

---
+++ Assets/OurProject/Scripts/ShootBullet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour {
+private Ray _ray;
+private RaycastHit _ratHit;
+[SerializeField]private GameObject _bullet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Aiming();
+		Shoot();
+	}
+
+    private void Shoot()
+    {
+        if(Input.GetMouseButtonDown(0))
+		{
+
+		}
+    }
+
+    private void Aiming()
+    {
+        throw new NotImplementedException();
+    }
+}
