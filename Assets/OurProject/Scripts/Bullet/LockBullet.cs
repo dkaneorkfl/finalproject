2c58cee2ef2ec853465f6420c3f178336e202036
dkaneorkfl <jtre159357@gmail.com>
2018-11-15 17:28:45 +0900

    1115 오후5:25
    Bullet을 상속을 위한 클래스로 변경.
    이 후 Bullet과 관련된 클래스는 모두 Bullet 폴더 안으로 옮김.
    
    탄 종류로
    일반 사격인 Normal
    조준 사격인 Lock
    회전 사격인 Curve
    유도 사격인 Guided를 작성
    
    EnermyShootBullet은 이제 탄의 종류를 고를 수 있게 되었으며, 탄의 종류에 따라 프리팹을 별개로 주게 되면서 기존에 탄의 역할을 하던 Sphere를 삭제하고 탄종에 따른 프리팹으로 분화됨.

---
+++ Assets/OurProject/Scripts/Bullet/LockBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockBullet : Bullet {
+
+	GameObject target;
+	RaycastHit hit;
+	// Use this for initialization
+	protected override void Start()
+	{
+		target = GameObject.Find("Capsule");
+		Destroy(gameObject,3);
+		transform.LookAt(target.transform);
+		GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward*100);
+	}
+}
---
+++ Assets/OurProject/Scripts/Bullet/NormalBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalBullet : Bullet {
+
+	// Use this for initialization
+	protected override void Start () 
+	{
+		Destroy(gameObject,3);
+		GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward*100);
+	}
+}
